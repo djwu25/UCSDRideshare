46a4c6a5340550323410ab0dcf1cd48e8da943dc
Duke Wu <63260716+djwu25@users.noreply.github.com>
2022-02-06 20:19:31 -0800

    Update views.py

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Post
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -7,5 +9,24 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template('home.html', user=current_user)
+
+@views.route('/add-ride', methods=['GET', 'POST'])
+@login_required
+def add_ride():
+    if request.method =='POST':
+        postTitle = request.form.get('post_title')
+        description = request.form.get('post_description')
+
+        if len(postTitle) < 1:
+            flash('Please include a title for the ride.', category='error')
+        elif len(description) < 1:
+            flash('Please include a description of the ride.', category='error')
+        else:
+            new_post = Post(title=postTitle, content=description, user_id=current_user)
+            db.session.add(new_post)
+            db.session.commit()
+            flash('Ticket created', category='success')
+            return redirect(url_for('views.home'))
+    return render_template('create_post.html', user=current_user)
 
     
