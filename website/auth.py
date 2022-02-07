e1dc78bfef1f10bccebeaee1e435bde7cd239faf
Duke Wu <63260716+djwu25@users.noreply.github.com>
2022-02-06 19:07:20 -0800

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -56,7 +56,7 @@ def sign_up():
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash('Account Created!', category='success')
             return redirect('/')
 
