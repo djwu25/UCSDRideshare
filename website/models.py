7e11dd5fc9e3c1defd83ea799086754533538019
Duke Wu <d3wu@ucsd.edu>
2022-02-18 19:43:10 -0800

    New Database Structure

--- website/models.py
+++ website/models.py
@@ -14,7 +14,12 @@ class User(db.Model, UserMixin):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+    depart_from = db.Column(db.String(100), nullable=False)
+    arrive_to = db.Column(db.String(100), nullable=False)
+    seats_available = db.Column(db.Integer, nullable=False)
+    seat_cost = db.Column(db.Integer, nullable=False)
+    time_of_departure = db.Column(db.Time, nullable=False)
+    date_of_departure = db.Column(db.Date, nullable=False)
+    extra_info = db.Column(db.Text, nullable=False)
 
--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import Post
@@ -13,21 +14,39 @@ def landing():
 @views.route('/rides')
 @login_required
 def home():
-    return render_template('home.html', user=current_user)
+    return render_template('home.html', user=current_user, data=db)
 
 @views.route('/add-ride', methods=['GET', 'POST'])
 @login_required
 def add_ride():
     if request.method =='POST':
         postTitle = request.form['post_title']
-        description = request.form['post_description']
+        departFrom = request.form['post_depart_from']
+        arriveTo = request.form['post_arrive_to']
+        seatsAvailable = request.form['post_seats_available']
+        seatCost = request.form['post_seat_cost']
+        timeOfDeparture = request.form['post_time']
+        dateOfDeparture = request.form['post_date']
+        extraInfo = request.form['post_extra_info']
 
         if len(postTitle) < 1:
             flash('Please include a title for the ride.', category='error')
-        elif len(description) < 1:
-            flash('Please include a description of the ride.', category='error')
+        elif len(departFrom) < 1:
+            flash('Please include location of departure.', category='error')
+        elif len(arriveTo) < 1:
+            flash('Please include arrival location.', category='error')
+        elif seatsAvailable is None:
+            flash('Please include the amount of available seats for this ride.', category='error')
+        elif seatCost is None:
+            flash('Please include a seat cost.', category='error')
+        elif timeOfDeparture is None:
+            flash('Please include a time of departure.', category='error')
+        elif dateOfDeparture is None:
+            flash('Please include a date of departure.', category='error')
         else:
-            new_post = Post(title=postTitle, content=description, user_id=current_user.id)
+            new_post = Post(title=postTitle, user_id=current_user.id, depart_from=departFrom, arrive_to=arriveTo, 
+                seats_available=seatsAvailable, seat_cost=seatCost, time_of_departure=timeOfDeparture,
+                date_of_departure=dateOfDeparture, extra_info=extraInfo)
             db.session.add(new_post)
             db.session.commit()
             flash('Ticket created', category='success')
